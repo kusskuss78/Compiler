0c05ea0acd873c369c616b084e0cca071ba7ea6c
kusskuss78 <xxth3saint9xx@gmail.com>
2020-01-21 11:14:13 -0500

    Started grammar class

---
+++ 01-regex/01-regex/Grammar.ts
@@ -0,0 +1,16 @@
+export class Grammar {
+    
+    
+    constructor(Gram: string) {
+        let s: Set<string> = new Set();
+        var input: string[] = Gram.split("\n");
+        
+        for (var i = 0; i < input.length; i++) {
+            
+        }
+
+       
+    }
+    
+
+}
