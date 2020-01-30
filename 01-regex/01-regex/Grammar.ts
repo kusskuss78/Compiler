9f7ecde1b792737f3137a5d631ae9b9a64603a8e
kusskuss78 <xxth3saint9xx@gmail.com>
2020-01-30 10:08:33 -0500

    Revert "Finished lab 1"
    This reverts commit 7bf739386e21bfebad39a04716be43058d467efc.

--- 01-regex/01-regex/Grammar.ts
+++ 01-regex/01-regex/Grammar.ts
@@ -1,30 +1,12 @@
 export class Grammar {
     
     
-    constructor(Gram: string)
-    {
+    constructor(Gram: string) {
         let s: Set<string> = new Set();
-        let input = Gram.split("\n");
+        var input: string[] = Gram.split("\n");
         
-        for (let i = 0; i < input.length; i++)
-        {
-            if(input[i]!='')
-            {
-                if (!input[i].includes(" -> "))
-                    throw new Error("No Identifiers");
-                let ID = input[i].split(" -> ");
-                if (s.has(ID[0]))
-                    throw new Error("Already has that variable");
-                s.add(ID[0]);
-                try {
-                    new RegExp(ID[1])
-                }
-                catch(e)
-                {
-                    throw new Error("Invalid regular expression")
-                }
-            }
-            
+        for (var i = 0; i < input.length; i++) {
+            //
         }
 
        
